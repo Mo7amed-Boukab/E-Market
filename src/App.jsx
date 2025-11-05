2b0b8d578b9e3aaf5990fa6191ae96b81c015ef2
mohamed <mohamedboukab202@gmail.com>
2025-11-05 16:13:15 +0100

    Initial commit: Setup du projet (Vite + React + TailwindCSS) et création de l’architecture du projet

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+     <div className="min-h-screen  bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Hello React + Vite + Tailwind!
+      </h1>
+    </div> 
+
+  )
+}
+
+export default App
