4b37b0ee387184f422d491b49d1ad242beacbe1d
mohamed <mohamedboukab202@gmail.com>
2025-11-09 03:25:59 +0100

    Set up environment variables for API URL and update axios instance configuration

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api/v2/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     Accept: "application/json",
